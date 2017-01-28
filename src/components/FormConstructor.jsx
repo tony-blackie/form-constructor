d7b6900ff7460e65ad64f392177a8fd0913b026e
MacBook <jim.casey321@gmail.com>
2017-01-28 14:41:14 +0200

    Change type of input to create depending on select element

--- src/components/FormConstructor.jsx
+++ src/components/FormConstructor.jsx
@@ -1,26 +1,39 @@
 import React, { Component } from 'react';
 
 export default class FormConstructor extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            formInputType: 'label'
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleClick(event) {
+        this.props.handleClick()
+    }
+
+    handleChange(event) {
+        this.setState({formInputType: event.target.value})
+    }
+
     render() {
         return (
             <div className="constructor">
                 <div className="constructor__component">
                     <label>Please, input the text you want to be inside the element</label>
-                    <div>
-                        <input type="radio" name="selectInputType" value="label" />
-                        <label>Label</label>
-                    </div>
-                    <div>
-                        <input type="radio" name="selectInputType" value="checkbox" />
-                        <label>Checkbox</label>
-                    </div>
 
-                    <div>
-                        <input type="radio" name="selectInputType" value="text" />
-                        <label>Text</label>
-                    </div>
+                    <select value={this.state.value} onChange={this.handleChange}>
+                        <option value="label">Label</option>
+                        <option value="checkbox">Checkbox</option>
+                        <option value="text">Text input</option>
+                    </select>
 
-                    <button>Add text input</button>
+                    <button onClick={this.handleClick}>Add text input</button>
                 </div>
             </div>
         );
