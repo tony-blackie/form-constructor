948b09da6e236eca3da01920d476c2712a7396ca
MacBook <jim.casey321@gmail.com>
2017-01-29 01:54:28 +0200

    Add callback for textarea change and some CSS

--- src/components/FormConfig.jsx
+++ src/components/FormConfig.jsx
@@ -11,21 +11,44 @@ export default class FormConfig extends Component {
         };
 
         this.toggleExample = this.toggleExample.bind(this);
+        this.handleTextareaChange = this.handleTextareaChange.bind(this);
     }
 
     toggleExample(event) {
         this.setState({isExampleVisible: !this.state.isExampleVisible});
     }
 
+    handleTextareaChange() {
+
+    }
+
     render() {
         return (
             <form className="config">
                 <h3>Paste JSON config here, to auto-generate form</h3>
-                <button onClick={this.toggleExample}>JSON format example</button>
 
-                <ExampleJSON />
+                <div className="config__example-wrapper">
+                    <button
+                        className="config__example-button"
+                        onClick={this.toggleExample}
+                        >
+                        Show JSON example
+                    </button>
+                </div>
 
-                <textarea placeholder="Paste your JSON here" className="config__textarea"></textarea>
+                {
+                    this.state.isExampleVisible ?
+                    <ExampleJSON />
+                    : null
+                }
+
+
+
+                <textarea
+                    placeholder="Paste your JSON here"
+                    className="config__textarea"
+                    onChange={this.handleTextareaChange}>
+                </textarea>
 
                 <div className="config__submit-wrapper">
                     <input type="submit" value="Submit JSON" className="config__submit" />
