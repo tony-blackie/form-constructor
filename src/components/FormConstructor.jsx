65cf3e152ec1b0e8845d455e008e4168ee98e892
MacBook <jim.casey321@gmail.com>
2017-01-28 14:22:30 +0200

    Add elements for adding form components

--- src/components/FormConstructor.jsx
+++ src/components/FormConstructor.jsx
@@ -3,7 +3,26 @@ import React, { Component } from 'react';
 export default class FormConstructor extends Component {
     render() {
         return (
-            <div>FormConstructor</div>
+            <div className="constructor">
+                <div className="constructor__component">
+                    <label>Please, input the text you want to be inside the element</label>
+                    <div>
+                        <input type="radio" name="selectInputType" value="label" />
+                        <label>Label</label>
+                    </div>
+                    <div>
+                        <input type="radio" name="selectInputType" value="checkbox" />
+                        <label>Checkbox</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="selectInputType" value="text" />
+                        <label>Text</label>
+                    </div>
+
+                    <button>Add text input</button>
+                </div>
+            </div>
         );
     }
 }
