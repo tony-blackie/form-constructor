393fe1793dd9294eb353d9612c21d78f410b0fe1
MacBook <jim.casey321@gmail.com>
2017-01-29 20:27:27 +0200

    Add tests for rendered markup in Form component

--- src/test/Form.spec.js
+++ src/test/Form.spec.js
@@ -5,8 +5,38 @@ import { expect } from 'chai';
 import Form from '../components/Form.jsx';
 
 describe('<Form/> tests', () => {
-    it('should render form element', () => {
-        const component = mount(<Form formElements={[]} />);
-        expect(component.find('form')).to.have.length(1);
+    let formElements = [
+        {
+            type: 'label',
+            value: '1337'
+        },
+        {
+            type: 'text',
+            value: '1337'
+        }
+    ];
+
+    it('should render 2 form elements', () => {
+        const component = mount(<Form formElements={formElements} />);
+        expect(component.find('.form__row')).to.have.length(2);
+    });
+
+    it('should render label', () => {
+        const formElements = [{type: 'label', value: '1337'}];
+        const component = mount(<Form formElements={formElements} />);
+        expect(component.find('label').html()).to.equal('<label>1337</label>');
+    });
+
+    it('should render text input', () => {
+        const formElements = [{type: 'text', value: '1337'}];
+        const component = mount(<Form formElements={formElements} />);
+        expect(component.find('input[type="text"]').html()).to.equal('<input type="text" placeholder="1337">');
+    });
+
+    it('should render checkbox', () => {
+        const formElements = [{type: 'checkbox', value: '1337'}];
+        const component = mount(<Form formElements={formElements} />);
+        expect(component.find('input[type="checkbox"]').html()).to.equal('<input type="checkbox">');
+        expect(component.find('span').html()).to.equal('<span>1337</span>');
     });
 });
