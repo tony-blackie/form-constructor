604a2c5602f8a75fbf35d53c8a18d01c53b77433
MacBook <jim.casey321@gmail.com>
2017-01-30 00:02:48 +0200

    Fix problem with already wrapped element

--- src/test/FormConfig.spec.js
+++ src/test/FormConfig.spec.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
 import chai, { expect } from 'chai';
+import sinon from 'sinon';
 
 import FormConfig from '../components/FormConfig.jsx';
 
 describe('<FormConfig/> tests', () => {
-    let component;
+    let component,
+        sandbox;
 
     beforeEach(() => {
+        sandbox = sinon.sandbox.create();
         component = shallow(<FormConfig/>);
+    });
+
+    afterEach(() => {
+        sandbox.restore();
     });
 
     it('should hide <ExampleJSON/> by default', () => {
@@ -20,5 +27,13 @@ describe('<FormConfig/> tests', () => {
         expect(component.find('ExampleJSON')).to.have.length(1);
         component.find('.config__example-button').simulate('click');
         expect(component.find('ExampleJSON')).to.have.length(0);
+    });
+
+    it('should call handleJSONSubmit on button click', () => {
+        const spy = sandbox.spy(FormConfig.prototype, 'handleJSONSubmit');
+
+        let newComponent = mount(<FormConfig />);
+        newComponent.find('.config__submit').simulate('click');
+        expect(spy.calledOnce).to.equal(true);
     });
 });
