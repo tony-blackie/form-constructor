2fe833cb3b20bce25684159e3fb1d1dd75c459b7
MacBook <jim.casey321@gmail.com>
2017-01-29 22:40:21 +0200

    Tests for FormConfig component

---
+++ src/test/FormConfig.spec.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import { expect } from 'chai';
+
+import FormConfig from '../components/FormConfig.jsx';
+
+describe('<FormConfig/> tests', () => {
+    let component;
+    beforeEach(() => {
+        component = shallow(<FormConfig/>);
+    });
+
+    it('should hide <ExampleJSON/> by default', () => {
+        expect(component.find('ExampleJSON')).to.have.length(0);
+    });
+
+    it('should toggle visibility of <ExampleJSON/> on button click', () => {
+        component.find('.config__example-button').simulate('click');
+        expect(component.find('ExampleJSON')).to.have.length(1);
+    });
+});
